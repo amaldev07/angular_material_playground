eb0cd5fcf96c58b0d2bbd7b695d326f8e550590c
Amaldev <amaldev.psn@gmail.com>
2021-11-09 20:12:20 +0530

    Code changes

---
+++ my-app/src/app/simple-mat-table/simple-mat-table.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+
+export interface PeriodicElement {
+  id: number;
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  { id: 1, name: 'Hydrogen', position: 1, weight: 1.0079, symbol: 'H' },
+  { id: 2, name: 'Helium', position: 2, weight: 4.0026, symbol: 'He' },
+];
+
+@Component({
+  selector: 'app-simple-mat-table',
+  templateUrl: './simple-mat-table.component.html',
+  styleUrl: './simple-mat-table.component.scss'
+})
+export class SimpleMatTableComponent {
+  displayedColumns: string[] = ['id', 'name', 'position', 'weight', 'symbol'];
+  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  ngOnInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+}
