967f9bc11db31bb64dc409bac5fa68466e24a190
Amaldev <amaldev.psn@gmail.com>
2025-06-07 01:13:15 +0530

    update autocomplete-multi-select.component.html and autocomplete-multi-select.component.ts

--- my-app/src/app/autocomplete-multi-select/autocomplete-multi-select.component.ts
+++ my-app/src/app/autocomplete-multi-select/autocomplete-multi-select.component.ts
@@ -7,4 +7,35 @@ import { Component } from '@angular/core';
 })
 export class AutocompleteMultiSelectComponent {
 
+  regions = [
+    'North America',
+    'South America',
+    'Europe',
+    'Asia',
+    'Africa',
+    'Australia',
+    'Antarctica'
+  ];
+  selectedRegions: string[] = [];
+
+
+  onSelectionChange(e: any) {
+
+    if (this.selectedRegions.length === this.regions.length) {
+      this.checked = true;
+    } else {
+      this.checked = false;
+    }
+  }
+
+
+  checked: boolean = false;
+  onCheckboxChange(event: any) {
+    if (event.checked) {
+      this.selectedRegions = [...this.regions];
+    } else {
+      this.selectedRegions = [];
+    }
+  }
 }
+
